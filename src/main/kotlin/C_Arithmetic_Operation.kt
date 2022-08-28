874912d780319709464a688d0afd18b69e931dd7
xtian-2000 <christian.gealone1@gmail.com>
2022-08-28 20:08:06 +0800

    - Added new arithmetic operations

--- src/main/kotlin/C_Arithmetic_Operation.kt
+++ src/main/kotlin/C_Arithmetic_Operation.kt
@@ -1,7 +1,11 @@
 fun main() {
     val x = 6
     val y = 7
-    val result = x + y
+    var result = x + y
 
     println(result)
+    result += 10
+    println(result)
+    println("The sum of $x and $y is: ${x + y}")
+    println("The sum of x and y is: $result")
 }
