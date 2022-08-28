9c75134172a2919c71453fa9afbe6cebd2d5ecbd
xtian-2000 <christian.gealone1@gmail.com>
2022-08-28 18:57:59 +0800

    - Changed value of stringVar

--- src/main/kotlin/B_Variable.kt
+++ src/main/kotlin/B_Variable.kt
@@ -1,11 +1,11 @@
 fun main() {
     val integerVar = 5              //Use val for variables that will not be modified
     val boolVar = true
-    var stringVar = "Hello World"   //Use var for variables that will be modified
+    var stringVar = "Hello World!"   //Use var for variables that will be modified
 
     println(integerVar)
     println(boolVar)
     println(stringVar)
-    stringVar = "Here's Kotlin"
+    stringVar = "Here's Kotlin!"
     println(stringVar)
 }
